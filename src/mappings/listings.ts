078e059771b688d984da00bf9197ac02674033b2
David Kajpust <kajpustd@gmail.com>
2019-01-18 23:33:11 -0500

    ListingUpdated mapping done

--- src/mappings/listings.ts
+++ src/mappings/listings.ts
@@ -67,6 +67,13 @@ export function handleListingCreated(event: ListingCreated): void {
   pinnedHashes.push('QmVKp9AHksCNdaHQKbMqetNVXUG3KVhFiASabXzogo2xD8')
   pinnedHashes.push('QmQFPod7GFneL7FrmZTxC6jr2P5mKv6RrW8GSQ7fSz2mdi')
 
+  // These are the initial Listings of the pinned Updated Listings
+  pinnedHashes.push('QmVAJ8U3QKrBzYdbBJBJnwYGeiVrYtjcMT8CqA4z4csKMK')
+  pinnedHashes.push('QmY8JXrf87hVufey9Rd1168iiLdrjmPK62yS6P86Foqf45')
+  pinnedHashes.push('QmZt5fi7Jfijz9EPRoxJSXtuBzadXtXskRSV7wch4DiU3X')
+
+
+
   // Only Run ipfs.cat() if it is a hardcoded base58 hash
   let i = pinnedHashes.indexOf(base58Hash)
   if (i != -1) {
@@ -74,6 +81,7 @@ export function handleListingCreated(event: ListingCreated): void {
     let data = json.fromBytes(getIPFSData).toObject()
     let ipfsListingData = new IPFSListingData(base58Hash)
     ipfsListingData.listingID = id
+    ipfsListingData.blockNumber = event.block.number
     ipfsListingData.schemaId = data.get('schemaId').toString()
     ipfsListingData.listingType = data.get('listingType').toString()
     ipfsListingData.category = data.get('category').toString()
@@ -142,49 +150,149 @@ export function handleListingCreated(event: ListingCreated): void {
   listing.save()
 }
 
-
-// TODO: delete this amongst all functions, it is wrong
-// let ipfsID = createIPFSDataID(id, listing.ipfsBytesHashes.length) // relateing to ipfs hashed, because I dont think you can get length from the derivedFrom length
-// let ipfsListingData = new IPFSListingData(ipfsID)
-// ipfsListingData.listingID = id
-// ipfsListingData.save()
-
+// This is treated as a full update to the Listing, as we are unable to tell which fields
+// were updated based on just on the IPFS hash
 export function handleListingUpdated(event: ListingUpdated): void {
   let id = event.params.listingID.toHex()
   let listing = Listing.load(id)
 
+  // Direct call the contract for deposit and depositManager
+  let smartContract = Marketplace.bind(event.address)
+  let storageListing = smartContract.listings(event.params.listingID)
+  listing.deposit = storageListing.value1
+
+  // Push to array to store IPFS hash (in bytes)
   let ipfsArray = listing.ipfsBytesHashes
   ipfsArray.push(event.params.ipfsHash)
   listing.ipfsBytesHashes = ipfsArray
 
-  //TODO direct call the contract for deposit
-  // todo - read the ipfs data
+  // Push to array to store IPFS hash (in base58)
+  let hexHash = addQm(event.params.ipfsHash) as Bytes
+  let base58Hash = hexHash.toBase58() // imported crypto function
+  let base58Array = listing.ipfsBase58Hashes
+  base58Array.push(base58Hash)
+  listing.ipfsBase58Hashes = base58Array
+
+  //////////////// JSON PARSING BELOW /////////////////////////////////////
+
+  // TODO: Remove eventually - as these are hardcoded files that I have pinned to IPFS, until we can reach their node
+  // Note, these are three hand picked hashes of ListingUpdates. The top ones
+  // are the original listing hashes, and get pinned in ListingCreated
+  let pinnedHashes = new Array<string>()
+  // pinnedHashes.push('QmVAJ8U3QKrBzYdbBJBJnwYGeiVrYtjcMT8CqA4z4csKMK')
+  pinnedHashes.push('QmYtWo8rBaYWK2xqWzrJvQv7mp949pM7aC2hz1YLTPtkKK')
+
+  // pinnedHashes.push('QmY8JXrf87hVufey9Rd1168iiLdrjmPK62yS6P86Foqf45')
+  pinnedHashes.push('QmXE1c445bqvSQo2p1S9w74QB7hJhEqvpzgpVmKDs6bg79')
+
+  // Note - this QmzT is stored in two different listings, and is overwritten in our db
+  // but that is okay since it is the exact same file
+  // pinnedHashes.push('QmZt5fi7Jfijz9EPRoxJSXtuBzadXtXskRSV7wch4DiU3X')
+  pinnedHashes.push('QmV7Qd8zDZv1pTtgmQgqKv8yfe8tHtKvqoc4uSMZ16dZgg')
+
+  // Only Run ipfs.cat() if it is a hardcoded base58 hash
+  let i = pinnedHashes.indexOf(base58Hash)
+  if (i != -1) {
+    let getIPFSData = ipfs.cat(base58Hash)
+    let data = json.fromBytes(getIPFSData).toObject()
+    let ipfsListingData = new IPFSListingData(base58Hash)
+    ipfsListingData.listingID = id
+    ipfsListingData.blockNumber = event.block.number
+    ipfsListingData.schemaId = data.get('schemaId').toString()
+    ipfsListingData.listingType = data.get('listingType').toString()
+    ipfsListingData.category = data.get('category').toString()
+    ipfsListingData.subCategory = data.get('subCategory').toString()
+    ipfsListingData.language = data.get('language').toString()
+    ipfsListingData.title = data.get('title').toString()
+    ipfsListingData.description = data.get('description').toString()
+    ipfsListingData.unitsTotal = data.get('unitsTotal').toBigInt()
+    ipfsListingData.media = []
+
+    // Creating Price
+    ipfsListingData.price = base58Hash
+    let priceObject = data.get('price').toObject()
+    let listingCurrency = new IPFSListingCurrency(base58Hash)
+    listingCurrency.amount = priceObject.get('amount').toString() // Can't use toBigInt(), since it is already coming in with kind STRING. so for now we store it as a string too
+    listingCurrency.currency = priceObject.get('currency').toString()
+    listingCurrency.save()
+
+    // Creating Commission, if it exists
+    let c = data.get('commission')
+    if (c != null) {
+      ipfsListingData.commission = base58Hash
+      let commissionObject = c.toObject()
+      let commission = new IPFSListingCommission(base58Hash)
+      commission.amount = commissionObject.get('amount').toString()
+      commission.currency = commissionObject.get('currency').toString()
+      commission.save()
+    }
+
+    // Creating CommissionPerUnit, if it exists
+    let cpu = data.get('commissionPerUnit')
+    if (cpu  != null) {
+      ipfsListingData.commissionPerUnit = base58Hash
+      let cpuObject = cpu.toObject()
+      let cpuEntity = new IPFSListingCommissionPerUnit(base58Hash)
+      cpuEntity.amount = cpuObject.get('amount').toString()
+      cpuEntity.currency = cpuObject.get('currency').toString()
+      cpuEntity.save()
+    }
+
+    // Creating dappSchemaId, if it exists
+    let dappSchemaId = data.get('dappSchemaId')
+    if (dappSchemaId != null){
+      ipfsListingData.dappSchemaId = dappSchemaId.toString()
+    }
+
+    // Creating the media array, if it exists
+    let media = data.get('media')
+    if (media != null){
+      let mediaArray = media.toArray()
+      for (let i = 0; i <mediaArray.length; i++){
+        let mediaID = BigInt.fromI32(i).toString().concat('-').concat(base58Hash)
+        let mediaEntity = new Media(mediaID)
+        mediaEntity.listing = base58Hash
+        let url = mediaArray[i].toObject().get('url').toString()
+        let contentType = mediaArray[i].toObject().get('contentType').toString()
+        mediaEntity.url = url
+        mediaEntity.contentType = contentType
+        mediaEntity.save()
+      }
+    }
+
+    ipfsListingData.save()
+  }
 
   listing.save()
-
-  let ipfsID = createIPFSDataID(id, listing.ipfsData.length)
-  let ipfsListingData = new IPFSListingData(ipfsID)
-  ipfsListingData.listingID = id
-  ipfsListingData.save()
-
 }
 
+// ListingWithdrawn, apparently simply just sets the units to 0, making the listing withdrawn
 export function handleListingWithdrawn(event: ListingWithdrawn): void {
   let id = event.params.listingID.toHex()
   let listing = Listing.load(id)
+
+  // Push to array to store IPFS hash (in bytes)
   let ipfsArray = listing.ipfsBytesHashes
   ipfsArray.push(event.params.ipfsHash)
   listing.ipfsBytesHashes = ipfsArray
-  listing.status = "withdrawn"
+  listing.status = "withdrawn" // TODO - do I need this? Unit of 0 implies ? Maybe not?
 
-  //TODO direct call the contract for deposit
-  // todo - read the ipfs data
+  // Push to array to store IPFS hash (in base58)
+  let hexHash = addQm(event.params.ipfsHash) as Bytes
+  let base58Hash = hexHash.toBase58() // imported crypto function
+  let base58Array = listing.ipfsBase58Hashes
+  base58Array.push(base58Hash)
+  listing.ipfsBase58Hashes = base58Array
+
+  // TODO - read the ipfs data. COMMENT OUT THE UPDATED ONE FOR NOW
+  // SHOULD ONLY UpDATE UNTIS TO ZERO !. can check myself
+  // but will i have to totally recreate and fill out ? or only update listing.status
+
+
+
+
   listing.save()
 
-  let ipfsID = createIPFSDataID(id, listing.ipfsData.length)
-  let ipfsListingData = new IPFSListingData(ipfsID)
-  ipfsListingData.listingID = id
-  ipfsListingData.save()
 
 }
 
@@ -199,14 +307,14 @@ export function handleListingArbitrated(event: ListingArbitrated): void {
   listing.ipfsBytesHashes = ipfsArray
   listing.status = "arbitrated"
 
-  //TODO direct call the contract for deposit
+  //TODO direct call the contract for deposit (TRUE! IT DOES UPDATE )
   // todo - read the ipfs data
   listing.save()
-
-  let ipfsID = createIPFSDataID(id, listing.ipfsData.length)
-  let ipfsListingData = new IPFSListingData(ipfsID)
-  ipfsListingData.listingID = id
-  ipfsListingData.save()
+  //
+  // let ipfsID = createIPFSDataID(id, listing.ipfsData.length)
+  // let ipfsListingData = new IPFSListingData(ipfsID)
+  // ipfsListingData.listingID = id
+  // ipfsListingData.save()
 
 }
 
@@ -226,17 +334,7 @@ export function handleListingData(event: ListingData): void {
   extraData.save()
 }
 
-// HELPERS
-
-
-// I DONT THINK THIS IS NEEDED, EACH IPFS DATA CAN JUST USE THE  IPFS HASH AS THE ID...
-
-function createIPFSDataID(entityID: string, ipfsDataLength: number): string {
-  // let ipfsID =  changetype<string>(ipfsDataLength as i32)
-  let ipfsID = BigInt.fromI32(ipfsDataLength as i32).toString()
-  let id = entityID.concat("-").concat(ipfsID)
-  return id
-}
+//////////// HELPERS /////////////////////////
 
 // Helper adding 0x12 and 0x20 to make the proper ipfs hash
 // the returned bytes32 is so [0,31]
